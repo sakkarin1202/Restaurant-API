fe544d6e54666dedcf89b67206e7fa3312f768c5
sakkarin1202 <xfake12340@hotmail.com>
2024-07-11 17:25:44 +0700

    update

--- controllers/restaurant.controller.js
+++ controllers/restaurant.controller.js
@@ -1,5 +1,6 @@
 const Restaurant = require("../models/restaurant.model");
 
+//create
 exports.create = async (req, res) => {
   const { name, type, imageUrl } = req.body;
   if (!name || !type || !imageUrl) {
@@ -9,7 +10,11 @@ exports.create = async (req, res) => {
   }
 
   try {
-    const restaurant = await Restaurant.findOne({ where: { name: name } });
+    const restaurant = await Restaurant.findOne({
+      where: {
+        name: name,
+      },
+    });
 
     if (restaurant) {
       return res.status(400).send({
@@ -38,6 +43,93 @@ exports.create = async (req, res) => {
       message:
         error.message ||
         "Something error occurred while creating the restaurant.",
+    });
+  }
+};
+
+//getAll
+exports.getAll = async (req, res) => {
+  await Restaurant.findAll()
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((error) => {
+      res.status(500).send({
+        message:
+          error.message ||
+          "Something error occurred while creating the restaurant.",
+      });
+    });
+};
+
+//getId
+exports.getById = async (req, res) => {
+  const id = req.params.id;
+  await Restaurant.findByPk(id)
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({ message: "No found Restarurant with id " + id });
+      } else {
+        res.send(data);
+      }
+    })
+    .catch((error) => {
+      res.status(500).send({
+        message:
+          error.message ||
+          "Something error occured while creating the restaurant.",
+      });
+    });
+};
+
+//Update
+exports.update = async (req, res) => {
+  const id = req.params.id;
+  await Restaurant.update(req.body, {
+    where: {
+      id: id,
+    },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.send({ message: "Restaurant was update successfully" });
+      } else {
+        res.send({
+          message:
+            "Cannot update restaurant with id" +
+            id +
+            ". Maybe restaurant was not found or res.body is empty!",
+        });
+      }
+    })
+    .catch((error) => {
+      res.status(500).send({
+        message:
+          error.massage ||
+          "Somthing error occured while creating the restaurant.",
+      });
+    });
+};
+
+// Delete
+exports.delete = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const num = await Restaurant.destroy({
+      where: { id: id },
+    });
+
+    if (num === 1) {
+      res.send({ message: "Restaurant was deleted successfully" });
+    } else {
+      res.send({
+        message: "Cannot delete restaurant with id=" + id + ".",
+      });
+    }
+  } catch (error) {
+    res.status(500).send({
+      message: "Error deleting restaurant with id=" + id,
+      error: error.message,
     });
   }
 };
--- routers/restaurant.router.js
+++ routers/restaurant.router.js
@@ -3,4 +3,8 @@ const router = express.Router();
 const restaurantController = require("../controllers/restaurant.controller");
 
 router.post("/", restaurantController.create);
+router.get("/", restaurantController.getAll);
+router.get("/:id", restaurantController.getById);
+router.put("/:id", restaurantController.update);
+router.delete("/:id", restaurantController.delete);
 module.exports = router;
